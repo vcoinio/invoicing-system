ab99291fd755a2fb81088725eefb7cf32133dbf0
vcoinio <bui.tien.1104@gmail.com>
2023-07-30 23:02:08 +0700

    add user auth

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\FruitsController;
 use App\Http\Controllers\InvoicesController;
 use App\Http\Controllers\HomeController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,43 +17,52 @@ use App\Http\Controllers\HomeController;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-//Home
-Route::get('/', [
-    HomeController::class, 'index'
-])->name('home');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-//Category
-Route::get('categories', [
-    CategoriesController::class,
-    'index'
-])->name('categories');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-Route::get('categories/create', [
-    CategoriesController::class,
-    'create'
-])->name('create');
+    //Home
+    Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::post('categories/create', [
-    CategoriesController::class, 'store'
-]);
+    //Category
+    Route::get('categories', [CategoriesController::class, 'index'])->name('categories');
 
-//Fruit
-Route::get(
-    'fruits',
-    [FruitsController::class, 'index']
-);
+    Route::get('categories/create', [CategoriesController::class, 'create'])->name('create');
 
-Route::get('fruits/create', [FruitsController::class, 'create']);
-Route::post('fruits/create', [FruitsController::class, 'store']);
+    Route::post('categories/create', [CategoriesController::class, 'store']);
 
-//Invoice
-Route::get('invoices', [
-    InvoicesController::class,
-    'index'
-])->name('invoces');
+    //Fruit
+    Route::get('fruits', [FruitsController::class, 'index']);
 
-Route::get('postCategory', [
-    PostCategoryController::class,
-    'PostIndex'
-])->name('postCategory');
+    Route::get('fruits/create', [FruitsController::class, 'create']);
+    Route::post('fruits/create', [FruitsController::class, 'store']);
+
+    //Invoice
+    Route::get('invoices', [InvoicesController::class, 'index'])->name('invoces');
+
+    Route::get('postCategory', [PostCategoryController::class, 'PostIndex'])->name('postCategory');
+});
+
+require __DIR__ . '/auth.php';
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
